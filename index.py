e70933614bd956adaa3c1b4009c4be4462a62b8a
ISeeJp <62587526+ISeeJp@users.noreply.github.com>
2023-06-03 15:07:07 -0400

    Example

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ app.layout = html.Div([
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    return 'daddy'
+    return 'mommy'
 
 
 if __name__ == '__main__':
