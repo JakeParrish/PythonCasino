d88b7e5b45dcdf645c90ac075b876345ecfe732e
ISeeJp <62587526+ISeeJp@users.noreply.github.com>
2023-06-03 14:40:34 -0400

    Initial page setup

---
+++ index.py
@@ -0,0 +1,21 @@
+from dash import html, dcc
+from dash.dependencies import Input, Output
+from app import app
+
+server = app.server
+
+app.layout = html.Div([
+            dcc.Location(id='url', refresh=True),
+            html.Div(id='page-content')
+        ])
+
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    return 'daddy'
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
